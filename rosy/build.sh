f6ebbbdbc0460337afdac6be2692aa3bfc22f658
NFS86 <jarbull86@gmail.com>
2022-04-09 23:56:36 +0000

    rosy update config to `SparkOS` [skip ci]

--- rosy/build.sh
+++ rosy/build.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest --depth 1 -b 12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -10,7 +10,7 @@ export TZ=Asia/Jakarta
 export USE_NFSparts=true # this is for NFSDev™ only
 export NFS_BUILD_VARIANT=normal # this is for NFSDev™ only
 # export NFS_BUILD_TYPE_GAPPS=coregapps # this is for NFSDev™ only
-lunch dot_rosy-userdebug
+lunch spark_rosy-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>${device_model}</b> Building Rom Started%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
-make bacon
+mka bacon
 # end
