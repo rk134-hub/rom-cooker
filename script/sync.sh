b2226815014e7413c33a977a15f6f83dfa30d9c8
NFS86 <jarbull86@gmail.com>
2022-04-10 00:57:20 +0000

    Rebase script

---
+++ script/sync.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+export my_dir="$HOME"
+export device="$(grep unch $my_dir/script/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)"
+export rom_name="$(grep init $my_dir/script/build_rom.sh -m 1 | cut -d / -f 4)"
+export command="$(head $my_dir/script/build_rom.sh -n $(expr $(grep '# build rom' $my_dir/script/build_rom.sh -n | cut -f1 -d:) - 1))"
+export only_sync=$(grep 'repo sync' $my_dir/script/build_rom.sh)
+
+
+mkdir -p $my_dir/ccache/$rom_name
+mkdir -p $my_dir/$rom_name
+cd $my_dir/$rom_name
+rm -rf .repo/local_manifests
+rm -rf sync.log
+bash -c "$command" |& tee -a $HOME/$rom_name/sync.log || true
+a=$(grep 'Cannot remove project' sync.log -m1|| true)
+b=$(grep "^fatal: remove-project element specifies non-existent project" sync.log -m1 || true)
+c=$(grep 'repo sync has finished' sync.log -m1 || true)
+d=$(grep 'Failing repos:' sync.log -n -m1 || true)
+e=$(grep 'fatal: Unable' sync.log || true)
+f=$(grep 'error.GitError' sync.log || true)
+g=$(grep 'error: Cannot checkout' sync.log || true)
+h=$(grep 'error: Downloading network changes failed.' sync.log || true)
+if [[ $a == *'Cannot remove project'* ]]
+then
+a=$(echo $a | cut -d ':' -f2 | tr -d ' ')
+rm -rf $a
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+elif [[ $b == *'remove-project element specifies non-existent'* ]]
+then exit 1
+elif [[ $c == *'repo sync has finished'* ]]
+then true
+elif [[ $d == *'Failing repos:'* ]]
+then
+d=$(expr $(grep 'Failing repos:' sync.log -n -m 1| cut -d ':' -f1) + 1)
+d2=$(expr $(grep 'Try re-running' sync.log -n -m1 | cut -d ':' -f1) - 1 )
+fail_paths=$(head -n $d2 sync.log | tail -n +$d)
+for path in $fail_paths
+do
+rm -rf $path
+aa=$(echo $path|awk -F '/' '{print $NF}')
+rm -rf .repo/project-objects/*$aa.git
+rm -rf .repo/projects/$path.git
+done
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+elif [[ $e == *'fatal: Unable'* ]]
+then
+fail_paths=$(grep 'fatal: Unable' sync.log | cut -d ':' -f2 | cut -d "'" -f2)
+for path in $fail_paths
+do
+rm -rf $path
+aa=$(echo $path|awk -F '/' '{print $NF}')
+rm -rf .repo/project-objects/*$aa.git
+rm -rf .repo/project-objects/$path.git
+rm -rf .repo/projects/$path.git
+done
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+elif [[ $f == *'error.GitError'* ]]
+then
+rm -rf $(grep 'error.GitError' sync.log | cut -d ' ' -f2)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+elif [[ $g == *'error: Cannot checkout'* ]]
+then
+echo hi
+coerr=$(grep 'error: Cannot checkout' sync.log | cut -d ' ' -f 4| tr -d ':')
+for i in $coerr
+do
+rm -rf .repo/project-objects/$i.git
+done
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+elif [[ $h == *'error: Downloading network changes failed.'* ]]
+then
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
+else
+exit 1
+fi
+rm -rf sync.log
---
+++ script/upload.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+export my_dir="$HOME"
+export device="$(grep unch $my_dir/script/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)"
+export rom_name="$(grep init $my_dir/script/build_rom.sh -m 1 | cut -d / -f 4)"
+export rel_date="$(date "+%Y%m%d")"
+export file_name="$(echo *${rel_date}*.zip)"
+export branch_name=$(grep init $my_dir/script/build.sh | awk -F "-b " '{print $2}' | awk '{print $1}')
+export rel_date="$(date "+%Y%m%d")"
+export shasum="out/target/product/$device/*$rel_date*.zip*sha*"
+export ota="out/target/product/$device/*ota*.zip"
+
+
+cd $my_dir/$rom_name
+rm -rf $shasum
+rm -rf $ota
+cd $my_dir/$rom_name/out/target/product/$device
+rclone copy --drive-chunk-size 256M --stats 1s ${file_name} NFS:$rom_name/$device -P
+
+cd $my_dir/$rom_name/out/target/product/$device
+telegram_message() {
+         curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" \
+         -d chat_id="${tg_id}" \
+         -d parse_mode="HTML" \
+         -d text="$1"
+}
+DL_LINK="https://rombuilder.projek.workers.dev/$rom_name/$device/$file_name"
+echo "=============================================="
+echo "Download Link: ${DL_LINK}" || { echo "ERROR: Failed to Upload the Build!"; }
+echo "=============================================="
+DATE_L="$(date +%d\ %B\ %Y)"
+DATE_S="$(date +"%T")"
+echo -e \
+"
+🚀 ${rom_name}
+
+✅ Build Completed Successfully!
+
+📱 Device: "${device_model}"
+🖥 Branch Build: "${branch_name}"
+⬇️ Download Link: <a href=\"${DL_LINK}\">Here</a>
+📅 Date: "$(date +%d\ %B\ %Y)"
+⏱ Time: "$(date +%T)"
+" > tg.html
+TG_TEXT=$(< tg.html)
+telegram_message "$TG_TEXT"
+echo " "
+rm -rf $my_dir/$rom_name/out/target/product/$device
+cd $my_dir
+rm -rf .repo*
