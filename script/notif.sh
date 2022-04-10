b8f8aec7b44f10754ea101252720bec561ed17a6
NFS86 <jarbull86@gmail.com>
2022-04-10 22:22:39 +0000

    Test

--- script/notif.sh
+++ script/notif.sh
@@ -4,9 +4,9 @@ export my_dir="$HOME"
 export device="$(grep unch $my_dir/script/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)"
 export rom_name="$(grep init $my_dir/script/build_rom.sh -m 1 | cut -d / -f 4)"
 export branch_name=$(grep init $my_dir/script/build_rom.sh | awk -F "-b " '{print $2}' | awk '{print $1}')
-cd $my_dir/$rom_name/out/target/product/$device
+cd $my_dir/$rom_name
 export rel_date="$(date "+%Y%m%d")"
-export file_name="$(echo *${rel_date}*.zip)"
+export file_name="$(basename out/target/product/$device/*.zip)"
 telegram_message() {
          curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" \
          -d chat_id="${tg_id}" \
@@ -31,6 +31,6 @@ TG_TEXT=$(< tg.html)
 telegram_message "$TG_TEXT"
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendSticker" -d sticker="CAACAgEAAx0CXv8ybAACHEJiUr3Z7W87PImIy1cs_dVTw2hdOAACqgADpAyuMtYfNzJONI2kIwQ" -d chat_id="${tg_id}"
 echo " "
-rm -rf $my_dir/$rom_name/out/target/product/$device
+#rm -rf $my_dir/$rom_name/out/target/product/$device
 cd $my_dir
 rm -rf .repo*
