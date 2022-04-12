80e27bf64f229198b88f6f6ae1eee65e08e8a513
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-04-12 20:42:53 +0700

    Test Build Evolution-X (rosy)

--- rosy/build_rom.sh
+++ rosy/build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest --depth 1 -b 12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -8,9 +8,9 @@ export NFS_BUILD_ROM_NAME=${rom_name} # this is for NFSDev™ only
 source build/envsetup.sh
 export TZ=Asia/Jakarta
 export USE_NFSparts=true # this is for NFSDev™ only
-export NFS_BUILD_VARIANT=overclock # this is for NFSDev™ only normal or overclock
+export NFS_BUILD_VARIANT=normal # this is for NFSDev™ only normal or overclock
 # export NFS_BUILD_TYPE_GAPPS=coregapps # this is for NFSDev™ only
-lunch spark_rosy-userdebug
+lunch evolution_rosy-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>Redmi 5 ($device)</b> Building Rom Started%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
-mka bacon
+mka evolution
 # end
