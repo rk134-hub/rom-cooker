bc636ac625e026620c0c0e84d0c605f061c8bed8
NFS86 <jarbull86@gmail.com>
2022-04-12 01:12:59 +0000

    SparkOS vip (rosy)

--- rosy/build_rom.sh
+++ rosy/build_rom.sh
@@ -8,7 +8,7 @@ export NFS_BUILD_ROM_NAME=${rom_name} # this is for NFSDev™ only
 source build/envsetup.sh
 export TZ=Asia/Jakarta
 export USE_NFSparts=true # this is for NFSDev™ only
-export NFS_BUILD_VARIANT=normal # this is for NFSDev™ only
+export NFS_BUILD_VARIANT=overclock # this is for NFSDev™ only normal or overclock
 # export NFS_BUILD_TYPE_GAPPS=coregapps # this is for NFSDev™ only
 lunch spark_rosy-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>Redmi 5 ($device)</b> Building Rom Started%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
