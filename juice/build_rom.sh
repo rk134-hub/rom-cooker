bbef438275d7f7aeae2558a7d5fdd12411b3a52d
c3eru <96016081+c3eru@users.noreply.github.com>
2022-04-15 19:50:48 +0700

    juice: bootleggers ify

--- juice/build_rom.sh
+++ juice/build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b sambunimbo -g default,-mips,-darwin,-notdefault
 git clone https://github.com/c3eru/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -9,11 +9,11 @@ export TZ=Asia/Jakarta
 export BUILD_USERNAME=mobxprjkt
 export BUILD_HOSTNAME=RANDOMBUILD
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch aosp_juice-userdebug
+lunch bootleg_juice-userdebug
  
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>POCO M3/9T ($device)</b> Building Rom Started%0A<b>ROM:</b>$rom_name%0A$(echo "${var_cache_report_config}")"
 
 mka bacon
  
 # end
-# set commit : rootdir
+# set commit : bootleggers ify
