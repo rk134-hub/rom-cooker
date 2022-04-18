6cebbb32edf584bab99da98102544639a4f903ca
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-04-18 21:58:31 +0700

    rosy: test again build corvus 😎

--- rosy/build_rom.sh
+++ rosy/build_rom.sh
@@ -10,6 +10,8 @@ export TZ=Asia/Jakarta
 export USE_NFSparts=true # this is for NFSDev™ only
 export NFS_BUILD_VARIANT=normal # this is for NFSDev™ only normal or overclock
 # export NFS_BUILD_TYPE_GAPPS=coregapps # this is for NFSDev™ only
+lunch corvus_rosy-eng
+lunch corvus_rosy-user
 lunch corvus_rosy-userdebug
 curl -s -X POST "https://api.telegram.org/bot${tg_token}/sendMessage" -d chat_id="${tg_id}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="===================================%0A<b>Redmi 5 ($device)</b> Building Rom Started%0A<b>ROM:</b>$rom_name%0A<b>Build Type:</b>$NFS_BUILD_VARIANT%0A$(echo "${var_cache_report_config}")"
 make corvus
