505dbc07ff9f4621c495a94e4c9e65f15bc50e4c
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-04-21 07:13:16 +0700

    Corvus rosy vip vanilla build

--- rosy/build_rom.sh
+++ rosy/build_rom.sh
@@ -11,7 +11,6 @@ export USE_NFSparts=true # this is for NFSDev™ only
 export NFS_BUILD_VARIANT=overclock # this is for NFSDev™ only normal or overclock
 # export NFS_BUILD_TYPE_GAPPS=coregapps # this is for NFSDev™ only
 export SELINUX_IGNORE_NEVERALLOWS=true
-export USE_GAPPS=true
 lunch corvus_rosy-eng
 lunch corvus_rosy-user
 lunch corvus_rosy-userdebug
