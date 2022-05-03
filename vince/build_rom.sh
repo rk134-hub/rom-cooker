f329d722a2fa07d8c4c9fdb0ba5ff3d673247e26
rk134 <rahul-k@bigdi.cc>
2022-05-03 17:12:48 +0100

    ok

--- vince/build_rom.sh
+++ vince/build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.gi
 git clone https://github.com/rk134/local_manifests.git --depth 1 -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
 
-# Build
+#Build
 source build/envsetup.sh
 export TZ=Asia/Jakarta
 lunch evolution_vince-user
